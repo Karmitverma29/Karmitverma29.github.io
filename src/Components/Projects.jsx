acce7db3ebce52e910e1b0484e5a55281ac21194
karmit <karmittech2004@gmail.com>
2023-01-27 12:19:55 +0530

    made some changes

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import "./projects.css"
 function Projects(){
-    return <div className="projects" style={{width:"60%",height:"1100px",margin:"auto",marginTop:"20px"}}>
+    return <div className="projects" style={{width:"60%",height:"auto",margin:"auto",marginTop:"20px"}}>
+
+<div  className="child" style={{border:"3px solid #ffb301",borderRadius:"10px",backgroundColor:"#ffffff"}}>
+<img className="image" style={{height:"50%",width:"100%",borderRadius:"10px"}} src={`${process.env.PUBLIC_URL}/buylance.png`} alt="img"/>
+<h1 className="heading" style={{fontSize:"24px",fontWeight:"600",textAlign:"center",marginTop:"10px",color:"#ffb301"}}>Buylance</h1>
+<p style={{textAlign:"center",fontSize:"16px"}}>
+An online one-stop fashion portal for apparel, footwear and accessories for men and women. 
+</p>
+<h2 style={{textAlign:"center",fontSize:"16px"}}><span style={{color:"#ffb301",fontWeight:"600"}}>Tech Stack:</span>
+React.js | TypeScript | Redux | Chakra UI
+
+</h2>
+<div className="btns" style={{height:"60px",width:"250px",margin:"auto",marginTop:"10px"}}>
+    <a href="https://buylance.netlify.app/" rel="noreferrer" target="_blank">
+<button className="live_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Live</button></a>
+<a href="https://github.com/faizal-siddiqui/murky-rate-2238" rel="noreferrer" target="_blank">
+
+<button className="code_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Souce code</button>
+</a>
+</div>
+
+
+</div>
 
 <div  className="child" style={{border:"3px solid #ffb301",borderRadius:"10px",backgroundColor:"#ffffff"}}>
 <img className="image" style={{height:"50%",width:"100%",borderRadius:"10px"}} src={`${process.env.PUBLIC_URL}/booking.png`} alt="img"/>
@@ -84,6 +106,8 @@ HTML | CSS | Javascript
 </a>
 </div>
 </div>
+
+
         </div>
 
 }
