c3637a912531bd75a5877d59f2a4f96ebc6c2b8a
karmit <karmittech2004@gmail.com>
2023-02-25 17:53:24 +0530

    added

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,115 +1,178 @@
 import React from "react";
-import "./projects.css"
-function Projects(){
-    return <div className="projects" style={{width:"60%",height:"auto",margin:"auto",marginTop:"20px"}}>
+import "./projects.css";
 
-<div  className="child" style={{border:"3px solid #ffb301",borderRadius:"10px",backgroundColor:"#ffffff"}}>
-<img className="image" style={{height:"50%",width:"100%",borderRadius:"10px"}} src={`${process.env.PUBLIC_URL}/buylance.png`} alt="img"/>
-<h1 className="heading" style={{fontSize:"24px",fontWeight:"600",textAlign:"center",marginTop:"10px",color:"#ffb301"}}>Buylance</h1>
-<p style={{textAlign:"center",fontSize:"16px"}}>
-An online one-stop fashion portal for apparel, footwear and accessories for men and women. 
-</p>
-<h2 style={{textAlign:"center",fontSize:"16px"}}><span style={{color:"#ffb301",fontWeight:"600"}}>Tech Stack:</span>
-React.js | TypeScript | Redux | Chakra UI
-
-</h2>
-<div className="btns" style={{height:"60px",width:"250px",margin:"auto",marginTop:"10px"}}>
-    <a href="https://buylance.netlify.app/" rel="noreferrer" target="_blank">
-<button className="live_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Live</button></a>
-<a href="https://github.com/faizal-siddiqui/murky-rate-2238" rel="noreferrer" target="_blank">
-
-<button className="code_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Souce code</button>
-</a>
-</div>
-
-
-</div>
-
-<div  className="child" style={{border:"3px solid #ffb301",borderRadius:"10px",backgroundColor:"#ffffff"}}>
-<img className="image" style={{height:"50%",width:"100%",borderRadius:"10px"}} src={`${process.env.PUBLIC_URL}/booking.png`} alt="img"/>
-<h1 className="heading" style={{fontSize:"24px",fontWeight:"600",textAlign:"center",marginTop:"10px",color:"#ffb301"}}>Replica of Booking.com</h1>
-<p style={{textAlign:"center",fontSize:"16px"}}>
-Booking.com BV provides online reservation services. 
-</p>
-<h2 style={{textAlign:"center",fontSize:"16px"}}><span style={{color:"#ffb301",fontWeight:"600"}}>Tech Stack:</span>
-HTML | CSS | JavaScript
-
-</h2>
-<div className="btns" style={{height:"60px",width:"250px",margin:"auto",marginTop:"10px"}}>
-    <a href="https://elastic-afterthought-7316.vercel.app/index.html" rel="noreferrer" target="_blank">
-<button className="live_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Live</button></a>
-<a href="https://github.com/Karmitverma29/elastic-afterthought-7316" rel="noreferrer" target="_blank">
-
-<button className="code_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Souce code</button>
-</a>
-</div>
-
-
-</div>
-<div className="child" style={{border:"3px solid #ffb301",borderRadius:"10px",backgroundColor:"#ffffff"}}>
-
-<img className="image"  style={{height:"50%",width:"100%",borderRadius:"10px"}} src={`${process.env.PUBLIC_URL}/nordstorm.png`} alt="img"/>
-<h1 className="heading" style={{fontSize:"24px",fontWeight:"600",textAlign:"center",marginTop:"10px",color:"#ffb301"}}>Replica of NordStorm (E-commerce)</h1>
-<p style={{textAlign:"center",fontSize:"16px"}}>
-Nordstrom is an American luxury department store that offers a wide variety of things. 
-
-</p>
-<h2 style={{textAlign:"center",fontSize:"16px"}}><span style={{color:"#ffb301",fontWeight:"600"}}>Tech Stack:</span>
-HTML | CSS | JavaScript
-</h2>
-
-<div className="btns" style={{height:"60px",width:"250px",margin:"auto",marginTop:"10px",backgroundColor:"#ffffff"}}>
-    <a href="https://skincare-delta.vercel.app/index.html" rel="noreferrer" target="_blank">
-<button className="live_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Live</button></a>
-<a href="https://github.com/srock2580/red-sail-4113" rel="noreferrer" target="_blank">
-
-<button className="code_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Souce code</button>
-</a>
-</div>
-
-</div>
-<div className="child" style={{border:"3px solid #ffb301",borderRadius:"10px",backgroundColor:"#ffffff"}}>
-<img className="image"  style={{height:"50%",width:"100%",borderRadius:"10px"}} src={`${process.env.PUBLIC_URL}/mailtrap.png`} alt="img"/>
-<h1 className="heading" style={{fontSize:"24px",fontWeight:"600",textAlign:"center",marginTop:"10px",color:"#ffb301"}}>Replica of Mailtrap</h1>
-<p style={{textAlign:"center",fontSize:"16px"}}>
-Mailtrap is a fake SMTP server designed to test, view, and share emails.
-</p>
-<h2 style={{textAlign:"center",fontSize:"16px"}}><span style={{color:"#ffb301",fontWeight:"600"}}>Tech Stack:</span>
-React.js | HTML | CSS | Javascript
-</h2>
-<div className="btns" style={{height:"60px",width:"250px",margin:"auto",marginTop:"10px"}}>
-    <a href="https://equal-part-8697-ps35.vercel.app/" rel="noreferrer" target="_blank">
-<button className="live_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Live</button>
-</a>
-<a href="https://github.com/Karmitverma29/equal-part-8697" rel="noreferrer" target="_blank">
-
-<button className="code_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Souce code</button>
-</a>
-</div>
-</div>
-<div className="child" style={{border:"3px solid #ffb301",borderRadius:"10px",backgroundColor:"#ffffff"}}>
-<img className="image"  style={{height:"50%",width:"100%",borderRadius:"10px"}} src={`${process.env.PUBLIC_URL}/rodan.png`} alt="img"/>
-<h1 className="heading" style={{fontSize:"24px",fontWeight:"600",textAlign:"center",marginTop:"10px",color:"#ffb301"}}>Replica of Rodan & Fields</h1>
-<p style={{textAlign:"center",fontSize:"16px"}}>
-Rodan & Fields, LLC, known as Rodan + Fields or R+F, is an American multi-level marketing company specializing in skincare products.
-</p>
-<h2 style={{textAlign:"center",fontSize:"16px"}}><span style={{color:"#ffb301",fontWeight:"600"}}>Tech Stack:</span>
-HTML | CSS | Javascript
-</h2>
-<div className="btns" style={{height:"60px",width:"250px",margin:"auto",marginTop:"10px"}}>
-    <a href="https://creative-moxie-5738eb.netlify.app/" rel="noreferrer" target="_blank">
-<button className="live_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Live</button>
-</a>
-<a href="https://github.com/tarunas033/wholesale-sky-2474" rel="noreferrer" target="_blank">
-
-<button className="code_btn" style={{height:"30px",width:"100px",border:"3px solid #ffb301",borderRadius:"10px",fontWeight:"600"}}>Souce code</button>
-</a>
-</div>
-</div>
-
-
+function Projects() {
+  return (
+    <div className="projects-container">
+      <div className="project-card">
+        <img
+          className="project-image"
+          src={`${process.env.PUBLIC_URL}/buylance.png`}
+          alt="Buylance project"
+        />
+        <h1 className="project-title">Buylance</h1>
+        <p className="project-description">
+          An online one-stop fashion portal for apparel, footwear and
+          accessories.
+        </p>
+        <h2 className="project-tech-stack">
+          <span className="project-tech-stack-title">Tech Stack:</span>
+          React.js | TypeScript | Redux | Chakra UI
+        </h2>
+        <div className="project-buttons">
+          <a
+            href="https://buylance.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-live-btn">Live</button>
+          </a>
+          <a
+            href="https://github.com/faizal-siddiqui/murky-rate-2238"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-code-btn">Source code</button>
+          </a>
         </div>
+      </div>
 
+      <div className="project-card">
+        <img
+          className="project-image"
+          src={`${process.env.PUBLIC_URL}/booking.png`}
+          alt="Replica of Booking.com project"
+        />
+        <h1 className="project-title">Replica of Booking.com</h1>
+        <p className="project-description">
+          Booking.com BV provides online reservation services.
+        </p>
+        <h2 className="project-tech-stack">
+          <span className="project-tech-stack-title">Tech Stack:</span>
+          HTML | CSS | JavaScript
+        </h2>
+        <div className="project-buttons">
+          <a
+            href="https://elastic-afterthought-7316.vercel.app/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-live-btn">Live</button>
+          </a>
+          <a
+            href="https://github.com/Karmitverma29/elastic-afterthought-7316"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-code-btn">Source code</button>
+          </a>
+        </div>
+      </div>
+
+      <div className="project-card">
+        <img
+          className="project-image"
+          src={`${process.env.PUBLIC_URL}/nordstorm.png`}
+          alt="Replica of NordStorm project"
+        />
+        <h1 className="project-title">Replica of NordStorm (E-commerce)</h1>
+        <p className="project-description">
+          Nordstrom is an American luxury department store that offers a wide
+          variety of things.
+        </p>
+        <h2 className="project-tech-stack">
+          <span className="project-tech-stack-title">Tech Stack:</span>
+          HTML | CSS | JavaScript
+        </h2>
+        <div className="project-buttons">
+          <a
+            href="https://elastic-afterthought-7316.vercel.app/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-live-btn">Live</button>
+          </a>
+          <a
+            href="https://github.com/Karmitverma29/elastic-afterthought-7316"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-code-btn">Source code</button>
+          </a>
+        </div>
+      </div>
+      <div className="project-card">
+        <img
+          className="project-image"
+          src={`${process.env.PUBLIC_URL}/mailtrap.png`}
+          alt="Replica of NordStorm project"
+        />
+        <h1 className="project-title">Replica of Mailtrap</h1>
+        <p className="project-description">
+        Mailtrap is a fake SMTP server designed to test, view, and share emails.
+
+        </p>
+        <h2 className="project-tech-stack">
+          <span className="project-tech-stack-title">Tech Stack:</span>
+          React.js | HTML | CSS | Javascript
+
+        </h2>
+        <div className="project-buttons">
+          <a
+            href="https://equal-part-8697-ps35.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-live-btn">Live</button>
+          </a>
+          <a
+            href="https://github.com/Karmitverma29/equal-part-8697"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-code-btn">Source code</button>
+          </a>
+        </div>
+      </div>
+      
+      <div className="project-card">
+        <img
+          className="project-image"
+          src={`${process.env.PUBLIC_URL}/rodan.png`}
+          alt="Replica of NordStorm project"
+        />
+        <h1 className="project-title">Replica of Rodan & Fields</h1>
+        <p className="project-description">
+        Rodan&Fields is an American multi-level marketing company of skincare products.
+
+        </p>
+        <h2 className="project-tech-stack">
+          <span className="project-tech-stack-title">Tech Stack:</span>
+          HTML | CSS | Javascript
+
+        </h2>
+        <div className="project-buttons">
+          <a
+            href="https://creative-moxie-5738eb.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-live-btn">Live</button>
+          </a>
+          <a
+            href="https://github.com/tarunas033/wholesale-sky-2474"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="project-code-btn">Source code</button>
+          </a>
+        </div>
+      </div>
+      
+      </div>
+      
+        
+);
 }
 
 export {Projects};
