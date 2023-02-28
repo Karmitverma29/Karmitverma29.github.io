f7a2ca092fbb5a962360f152e6257b817c5d003b
karmit <karmittech2004@gmail.com>
2023-02-28 09:39:13 +0530

    made changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,34 +1,41 @@
 import React from "react";
 import Intro from "./Intro";
 import AvatarWithRipple from "./Avatar";
-import { Button } from '@chakra-ui/react';
-function Home(){
-    let change=(e)=>{
+import { Button } from "@chakra-ui/react";
+import "./home.css";
 
-        e.target.style.background = '#ffffff';
-        e.target.style.border='1px solid #ffb301 '
-
-    }
-    let OnLeave=(e)=>{
-        e.target.style.background = '#ffb301';
-
-    }
-    return <div id="home">
-        <div style={{height:"550px",width:"100%",textAlign:"center"}}>
-        
-<h1 style={{textAlign:"center",fontSize:"35px",fontWeight:"600",marginTop:"70px",color:"black"}}>Hi, I am Karmit Verma,</h1>
-<h2 style={{textAlign:"center",fontSize:"35px",fontWeight:"600",marginTop:"20px",color:"#ffb301"}}>{<Intro/>}</h2>
-<p  style={{textAlign:"center",fontSize:"18px",fontWeight:"600",color:"#black",marginTop:"10px"}}>Passionate full-stack web developer with strong technical and <br /> non-technical skills and a commitment to building useful, <br /> interactive web applications.</p>
-<AvatarWithRipple/>
- <Button as="a" href="/Karmit_Verma_Resume.pdf" download onClick={()=>{
-    window.open("https://drive.google.com/file/d/1vltTnwRyXTRXc4L7nuANgQXYJqkiVHGP/view?usp=sharing", "_blank")
- }}  onMouseLeave={OnLeave}    onMouseEnter={change} style={{backgroundColor:"#ffb301"}}  >Resume ⮞</Button>
-</div>
-
-
-
-
-</div>
+function Home() {
+  return (
+    <div id="home">
+      <div className="home-content">
+        <h1 className="home-title">Hi, I am Karmit Verma,</h1>
+        <h2 className="home-subtitle">
+          <Intro />
+        </h2>
+        <p className="home-description">
+          Passionate full-stack web developer with strong technical and
+          non-technical skills and a commitment to building useful, interactive
+          web applications.
+        </p>
+        <AvatarWithRipple />
+        <Button
+          as="a"
+          href="/Karmit_Verma_Resume.pdf"
+          download
+          onClick={() => {
+            window.open(
+              "https://drive.google.com/file/d/1vltTnwRyXTRXc4L7nuANgQXYJqkiVHGP/view?usp=sharing",
+              "_blank"
+            );
+          }}
+          className="home-button"
+          sx={{ backgroundColor: "#ffb301 !important" }}
+        >
+          Resume ⮞
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export {Home};
+export { Home };
